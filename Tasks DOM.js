6588df531b3185a9900285995e8ab61c10c8d18b
dezmunt1 <34319509+dezmunt1@users.noreply.github.com>
2018-12-26 19:07:14 +0300

    Update Tasks DOM.js

--- Tasks DOM.js
+++ Tasks DOM.js
@@ -1,1 +1,8 @@
-functiom,
+1.https://learn.javascript.ru/traversing-dom#dom-children
+console.log(document.head);
+console.log(document.body.children[1]);
+console.log(document.body.children[1].lastElementChild);
+
+2.https://learn.javascript.ru/traversing-dom#proverka-suschestvovaniya-detey
+
+
