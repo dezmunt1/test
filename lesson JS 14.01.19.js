4e37cff7172940a469634ab742b9d97919b23a51
dezmunt1 <34319509+dezmunt1@users.noreply.github.com>
2019-01-15 01:22:44 +0300

    Create lesson JS 14.01.19.js

---
+++ lesson JS 14.01.19.js
@@ -0,0 +1,41 @@
+// 1. https://learn.javascript.ru/task/closure-sum
+function sum(a) {
+  return (b) => {
+    return console.log(a+b);
+  };
+}
+
+sum(10)(12);
+
+// 2. https://learn.javascript.ru/task/stringbuffer
+function makeBuffer() {
+    let privateValue = new String();
+  return function(value) {
+    if (arguments.length === 0) return console.log(privateValue);
+    privateValue += value;    
+  };
+}
+
+let buffer = makeBuffer();
+buffer(1);
+buffer(2);
+buffer('d');
+buffer();
+
+// 3.https://learn.javascript.ru/task/stringbuffer-with-clear
+function makeBuffer() {
+    let privateValue = '';
+
+    function buffer(value) {
+        if (arguments.length === 0) return console.log(privateValue);
+        privateValue += value;
+    }
+
+    buffer.clear = () => {
+        privateValue = '';
+    };
+    return buffer;
+}
+
+//4. https://learn.javascript.ru/task/make-army
+
