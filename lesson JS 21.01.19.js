b5f9d816c1e53d62876baf51ea575db6eb3d8e61
dezmunt1 <34319509+dezmunt1@users.noreply.github.com>
2019-01-23 00:45:18 +0300

    Update lesson JS 21.01.19.js

--- lesson JS 21.01.19.js
+++ lesson JS 21.01.19.js
@@ -10,3 +10,18 @@ function debounce(f, ms) {
         }, ms)
       }
     }
+
+// 2. https://learn.javascript.ru/task/throttle
+
+function throttle(f, ms) {
+  let timeId;
+  return function(arg) {
+    if (!timeId) {     // Если не разу не вызывалось
+      f.call(this, arg);
+    }
+    clearTimeout( timeId );
+    timeId = setTimeout(function(){
+      f.call(this, arg);
+    }, ms)
+  }
+}
