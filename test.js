edd63d65354b6ddf2fd0feb0fb4ad1709139b80d
dezmunt1 <34319509+dezmunt1@users.noreply.github.com>
2019-02-23 12:03:12 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-////
+// поспрашиваем яндекс
+// <тип_метода> https://api.partner.market.yandex.ru/v<версия_API>/<ресурс>.<формат_ответа>?<параметры>
+
+let inf = {
+  token: prompt('Введите токен'),
+  id: prompt('Введите ID приложения'),
+};
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://api.partner.market.yandex.ru/v2/regions/213.json');
+xhr.setRequestHeader('Authorization', `OAuth oauth_token="${inf.token}", oauth_client_id="${inf.id}"`);
+xhr.setRequestHeader('Content-Type', 'application/json');
+
+
+xhr.send();
+
+xhr.addEventListener('load', (e)=>{
+  console.log(e.target);
+});
